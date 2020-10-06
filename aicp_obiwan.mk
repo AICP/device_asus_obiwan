40d7a9e3e82cd2c4a156fea023d0a3633909e4ae
mosimchah <mosimchah@gmail.com>
2020-10-05 21:40:39 -0700

    AICP-ify
    Change-Id: I800b5fb462f4f2097fe62680a7ee063976f12c99

--- aicp_obiwan.mk
+++ aicp_obiwan.mk
@@ -21,14 +21,14 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from instantnoodlep device
 $(call inherit-product, device/asus/obiwan/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Aicp stuff.
+$(call inherit-product, vendor/aicp/config/common_full_phone.mk)
 
 PRODUCT_BRAND := asus
 PRODUCT_DEVICE := obiwan
 PRODUCT_MANUFACTURER := asus
 PRODUCT_MODEL := ASUS_I003D
-PRODUCT_NAME := lineage_obiwan
+PRODUCT_NAME := aicp_obiwan
 
 PRODUCT_GMS_CLIENTID_BASE := android-asus
 
@@ -39,3 +39,10 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     TARGET_DEVICE=ZS661KS
 
 BUILD_FINGERPRINT := "asus/WW_I003D/ASUS_I003_1:11/RKQ1.200710.002/18.0410.2109.172-0:user/release-keys"
+
+# AICP Device Maintainers
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    DEVICE_MAINTAINERS="Moshe Barash (mosimchah)"
+
+PRODUCT_OVERRIDE_INFO := true
+PRODUCT_OVERRIDE_FINGERPRINT := asus/WW_I003D/ASUS_I003_1:11/RKQ1.200710.002/18.0410.2109.172-0:user/release-keys
